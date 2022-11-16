52657148277ac195a765dc026970a65616c5d04c
jagodawojcik <wojcik.jagoda2@gmail.com>
2022-11-16 12:19:37 +0000

    algo needs tidying up

--- mdpAgents.py
+++ mdpAgents.py
@@ -1,33 +1,3 @@
-# mdpAgents.py
-# parsons/20-nov-2017
-#
-# Version 1
-#
-# The starting point for CW2.
-#
-# Intended to work with the PacMan AI projects from:
-#
-# http://ai.berkeley.edu/
-#
-# These use a simple API that allow us to control Pacman's interaction with
-# the environment adding a layer on top of the AI Berkeley code.
-#
-# As required by the licensing agreement for the PacMan AI we have:
-#
-# Licensing Information:  You are free to use or extend these projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
-# Attribution Information: The Pacman AI projects were developed at UC Berkeley.
-# The core projects and autograders were primarily created by John DeNero
-# (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
-# Student side autograding was added by Brad Miller, Nick Hay, and
-# Pieter Abbeel (pabbeel@cs.berkeley.edu).
-
-# The agent here is was written by Simon Parsons, based on the code in
-# pacmanAgents.py
-
 from pacman import Directions
 from game import Agent
 import mapAgents
@@ -36,7 +6,7 @@ import random
 import game
 import util
 
-
+#Rewards
 R_FOOD = 10
 R_WALLS = None
 R_CAP = 5
@@ -46,42 +16,145 @@ R_PAC = -15
 R_EMPTY = 0
 T_VALUE = 500
 
+#Interface
+class MDP:
+#     #""" Return all states of this MDP """
+#     def get_states(self):
+        #return all possible states as (x,y) coordinates
+
+#     #""" Return all actions with non-zero probability from this state """
+    def get_actions(self, state):
+        #all legal actions for the current state
+        return state.getLegalPacmanActions()
+
+#     """ Return the discount factor for this MDP """
+    def get_discount_factor(self):
+        discount_factor = 0.9
+        return discount_factor
+
+#    Return the initial state of this MDP
+    def get_initial_state(state):
+        return api.whereAmI(state)
+
+#Return all non-zero probability transitions for this action
+# from this state, as a list of (next_state, probability) pairs
+    def get_transitions(self, state, action):
+        transitions = []
+
+        # Probabilities
+        straight = api.directionProb
+        noise = 0.5*(1 - api.directionProb)
+
+        (x, y) = api.whereAmI(state) #initial state
+
+        if action == Directions.NORTH:
+            transitions += self.valid_add(state, (x, y + 1), straight) #goes up as intented
+            transitions += self.valid_add(state, (x - 1, y), noise) #goes left
+            transitions += self.valid_add(state, (x + 1, y), noise)
+            transitions += self.valid_add(state, (x, y-1), 0.0)  
+
+        elif action == Directions.SOUTH:
+            transitions += self.valid_add(state, (x, y - 1), straight)
+            transitions += self.valid_add(state, (x - 1, y), noise)
+            transitions += self.valid_add(state, (x + 1, y), noise)
+
+        elif action == Directions.RIGHT:
+            transitions += self.valid_add(state, (x + 1, y), straight)
+            transitions += self.valid_add(state, (x, y - 1), noise)
+            transitions += self.valid_add(state, (x, y + 1), noise)
+
+        elif action == Directions.LEFT:
+            transitions += self.valid_add(state, (x - 1, y), straight)
+            transitions += self.valid_add(state, (x, y - 1), noise)
+            transitions += self.valid_add(state, (x, y + 1), noise)
+
+        return transitions
+    
+    #when wall in place there will be some probability of staying in the same state
+    def valid_add(self, state, new_state, probability):
+
+        if new_state not in MDP.get_states(self):
+            return [(api.whereAmI(state), probability)]
+
+        return [(new_state, probability)]
+     
+
+#""" Return the reward for transitioning from state to nextState via action
+# return numeric reward for this state given some action
+    def get_reward(self, state, action, new_state):
+        reward = 0.0
+        #here modify to assign to reward whatever value appropriate
+        
+        if state in self.get_goal_states().keys():
+            reward = self.get_goal_states().get(state) 
+        else:
+            reward = self.action_cost #if state not in goal states assign some reward
+        return reward
+
+
+
+# initialize the values:
+# Values is the length of how many states we've got
+#like an additional external array V = [0, 0, 0 ... 0s]
+#pi = [None, none, none , .... nones] size of states, same as values stores best policies
+class ValueIteration:
+
+    def value_iteration(self, max_iterations=100, theta=0.001):
+
+        for i in range(max_iterations):
+            delta = 0.0
+            new_values = dict([(s, 0) for s in mdp.states]) #initialize values for each state assign zero
+            for state in self.mdp.get_states():
+                max_val = 0 #initialise max value to zero
+                #for each state, compute value for each legal action
+                for action in self.mdp.get_actions(state):
+                    # Calculate the state value when certain action is taken
+                    for (new_state, probability) in self.mdp.get_transitions(state, action):
+                        reward = self.mdp.get_reward(state, action, new_state) #direct reward for that resulting next state
+                        #U1[s] = prob * (reward(s) + gamma* val_newstate)
+                        new_value += probability * (reward + (_discount_factor()
+                                * self.Values(new_state) #value for next state
+                            )
+                        )
+
+                    # Store the value for best action so far
+                    max_val = max(max_val, new_value)
+
+                    #update best policy
+                    if Values(state) < new_value:
+                        pi[state] = get_actions[action] #store action with highest value
+                #update value with highest value
+                new_values[state] = max_val
+                #update max difference
+                delta = max(delta, abs(values[s] - new_value[s]))
+            #update (extr. vals function)
+            values = new_values
+
+            # Terminate if the value function has converged
+            if delta < theta:
+                return i
+
+
+#write code to perform action based on the optimal policy
+
+
+
 
 
 class MDPAgent(Agent):
-
-    def getHeight(self, corners):
-        #save the highest corners[][1] coord as a height
-        self.height = -1
-        for i in range(len(corners)):
-            if corners[i][1] > self.height:
-                self.height = corners[i][1]
-        self.height = self.height+1
-        return self.height
-
-    def getWidth(self, corners):
-        #save the highest corners[][0] coord as a height
-        self.width = -1
-        for i in range(len(corners)):
-            if corners[i][0] > self.width:
-                self.width = corners[i][0]
-        self.width = self.width+1
-        return self.width
 
     # Constructor: this gets run when we first invoke pacman.py
     def __init__(self):
         print "Starting up MDPAgent!"
         name = "Pacman"
-      
-
+        
 
     # Gets run after an MDPAgent object is created and once there is
     # game state to access.
     def registerInitialState(self, state):
         print "Running registerInitialState for MDPAgent!"
-        print "I'm at:"
-        
-        
+
+
     def initialStatesMap(self, state):
         width = self.getWidth(api.corners(state))
         height = self.getHeight(api.corners(state))
@@ -139,51 +212,3 @@ class MDPAgent(Agent):
             legal.remove(Directions.STOP)
         # Random choice between the legal options.
         return api.makeMove(random.choice(legal), legal)
-
-
-    def get_transitions(self, state, action):
-        transitions = []
-
-        if state == self.TERMINAL:
-            if action == self.TERMINATE:
-                return [(self.TERMINAL, 1.0)]
-            else:
-                return []
-
-    def value_iteration(self, max_iterations=100, theta=0.001):
-
-        for i in range(max_iterations):
-            delta = 0.0
-            new_values = # here initial values
-            for state in self.mdp.get_states(): #here go my states
-                qtable = QTable() # here save calculated values
-                for action in self.mdp.get_actions(state):
-                    # Calculate the value of Q(s,a)
-                    new_value = 0.0
-                    #"" get_transitions - Return all non-zero probability transitions for this action
-        #from this state, as a list of (state, probability) pairs
-                    for (new_state, probability) in self.mdp.get_transitions( #
-                        state, action
-                    ):
-                        reward = self.mdp.get_reward(state, action, new_state) #Return the reward for transitioning from state to nextState via action
-                        new_value += probability * (
-                            reward
-                            + (
-                                self.mdp.get_discount_factor()
-                                * self.values.get_value(new_state)
-                            )
-                        )
-
-                    qtable.update(state, action, new_value)
-
-                # V(s) = max_a Q(sa)
-                (_, max_q) = qtable.get_max_q(state, self.mdp.get_actions(state))
-                delta = max(delta, abs(self.values.get_value(state) - max_q))
-                new_values.update(state, max_q)
-
-            self.values.merge(new_values)
-
-            # Terminate if the value function has converged
-            if delta < theta:
-                return i
-
